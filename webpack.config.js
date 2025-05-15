b6e4ae981acd47195e141a06180111655bb50636
FlapJack100 <Den_lion_17@mail.ru>
2025-05-16 01:00:28 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/Weather/',
-    clean: true,
-  },
-  devServer: {
-    static: './dist',
-    port: 3000,
-    open: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Погода',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/Weather/', 
+        clean: true,
+    },
+    devServer: { 
+        static: './dist',
+        port: 3000,
+        open: true,
+        historyApiFallback: true, 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ 
+            title: 'Погода',
+            template: './src/index.html'
+        })
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], 
+            }
+        ]
+    }
 };
